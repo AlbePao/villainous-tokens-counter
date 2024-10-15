85179615d1071cf4f7968ff5f15479e0c12cf5eb
Alberto Paolucci <alberto.paolucci@icloud.com>
2024-10-15 16:58:35 +0200

    add vite-plugin-pwa

---
+++ manifestPlugin.ts
@@ -0,0 +1,44 @@
+import { VitePWAOptions } from 'vite-plugin-pwa';
+
+// you can copy the base structure of manifest object.
+export const manifestForPlugin: Partial<VitePWAOptions> = {
+  registerType: 'prompt',
+  includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'masked-icon.svg'],
+  manifest: {
+    name: 'Villainous Tokens Counter',
+    short_name: 'villainous-tokens-counter',
+    description: 'I am a simple vite app',
+    icons: [
+      {
+        src: '/android-chrome-192x192.png',
+        sizes: '192x192',
+        type: 'image/png',
+        purpose: 'favicon',
+      },
+      {
+        src: '/android-chrome-512x512.png',
+        sizes: '512x512',
+        type: 'image/png',
+        purpose: 'favicon',
+      },
+      {
+        src: '/apple-touch-icon.png',
+        sizes: '180x180',
+        type: 'image/png',
+        purpose: 'apple touch icon',
+      },
+      {
+        src: '/maskable_icon.png',
+        sizes: '512x512',
+        type: 'image/png',
+        purpose: 'any maskable',
+      },
+    ],
+    theme_color: '#171717',
+    background_color: '#0a0a0a',
+    display: 'standalone',
+    scope: '/',
+    start_url: '/',
+    orientation: 'portrait',
+  },
+};
