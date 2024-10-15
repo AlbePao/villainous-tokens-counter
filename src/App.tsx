1ed10c4cd1c30675066b8e991f5428e4f1596ff1
Alberto Paolucci <alberto.paolucci@icloud.com>
2024-10-15 18:17:11 +0200

    updates

--- src/App.tsx
+++ src/App.tsx
@@ -1,48 +1,34 @@
 import '@fontsource/pirata-one';
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import { useWakeLock } from 'react-screen-wake-lock';
 import { VillainCard } from './components/VillainCard';
 import { VillainDashboard } from './components/VillainDashboard';
 import { Villain } from './models/villain';
 import { villains } from './models/villains-list';
 
-// TODO: read https://medium.com/@rakeshdhariwal61/converting-your-react-vite-app-into-pwa-d7211e9cd0c5
 function App() {
-  const { isSupported, released, request, release } = useWakeLock({
-    onRequest: () => alert('Screen Wake Lock: requested!'),
-    onError: () => alert('An error happened 💥'),
-    onRelease: () => alert('Screen Wake Lock: released!'),
-  });
+  const { request, release } = useWakeLock();
 
   const [villain, setVillain] = useState<Villain | null>(null);
 
-  // TODO: add fake spinner
-  // TODO: get current villain from localStorage, if null, show villains list
-  // TODO: if villain is selected, prevent reloading and tab close
+  const aaaVillain = (villain: Villain | null) => {
+    if (villain) {
+      request();
+    } else {
+      release();
+    }
 
-  useEffect(() => {}, [villain]);
-
-  useEffect(() => {}, []);
+    setVillain(villain);
+  };
 
   return (
     <>
-      <div className='text-white'>
-        <p>
-          Screen Wake Lock API supported: <b>{`${isSupported}`}</b>
-          <br />
-          Released: <b>{`${released}`}</b>
-        </p>
-        <button type='button' onClick={() => (released === false ? release() : request())}>
-          {released === false ? 'Release' : 'Request'}
-        </button>
-      </div>
-
       {villain ? (
-        <VillainDashboard {...villain} onCancel={() => setVillain(null)} />
+        <VillainDashboard {...villain} onCancel={() => aaaVillain(null)} />
       ) : (
         <div className='w-full mx-auto md:max-w-[67.5rem] grid grid-cols-2  md:grid-cols-3'>
           {villains.map((villain) => (
-            <VillainCard key={villain.name} {...villain} onClick={() => setVillain(villain)} />
+            <VillainCard key={villain.name} {...villain} onClick={() => aaaVillain(villain)} />
           ))}
         </div>
       )}
