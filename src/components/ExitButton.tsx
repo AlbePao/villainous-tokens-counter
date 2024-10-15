5b9c6af1dd631696ccd55c0fcf168e4f8d392b18
Alberto Paolucci <alberto.paolucci@icloud.com>
2024-10-15 14:58:36 +0200

    Svg components  implementations

--- src/components/ExitButton.tsx
+++ src/components/ExitButton.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
-import closeIcon from '../assets/close.svg';
-import fateIcon from '../assets/thunderstorm.svg';
+import { SvgCheck } from './SvgCheck';
+import { SvgClose } from './SvgClose';
 
 export const ExitButton = () => {
   const [confirmOpened, toggleConfirm] = useState(false);
@@ -8,9 +8,27 @@ export const ExitButton = () => {
   return (
     <>
       {confirmOpened && (
-        <div className='absolute top-0 bottom-0 left-0 right-0 flex items-center justify-center'>
-          <div className='flex items-center justify-center rounded-full shadow-lg w-36 h-36 bg-slate-50/90 shadow-yellow-500/30'>
-            <img src={fateIcon} width='88' height='88' alt='Gettone Fato' />
+        <div className='fixed top-0 bottom-0 left-0 right-0 z-50 flex items-center justify-center bg-neutral-900/90 '>
+          <div className='flex flex-col items-center justify-center w-11/12 p-8 shadow-lg max-w-96 rounded-3xl bg-neutral-900/90 shadow-yellow-500/30'>
+            <div className='mb-6 text-2xl text-center text-slate-50'>
+              Se esci, i progressi del tuo cattivo andranno perduti.
+              <br />
+              Confermi di volere uscire?
+            </div>
+            <div className='flex flex-row justify-around w-full'>
+              <button
+                onClick={() => toggleConfirm(false)}
+                className='flex items-center justify-center w-16 h-16 border rounded-full shadow-md bg-gray-950 left-4 shadow-red-500/60 active:shadow-red-500 border-red-600/40 active:border-red-600/60'
+              >
+                <SvgClose width='48' height='48' className='fill-red-800' />
+              </button>
+              <button
+                onClick={() => toggleConfirm(true)}
+                className='flex items-center justify-center w-16 h-16 border rounded-full shadow-md bg-gray-950 left-4 shadow-green-500/60 active:shadow-green-500 border-green-600/40 active:border-green-600/60'
+              >
+                <SvgCheck width='48' height='48' className='fill-green-800' />
+              </button>
+            </div>
           </div>
         </div>
       )}
@@ -19,7 +37,7 @@ export const ExitButton = () => {
         onClick={() => toggleConfirm(true)}
         className='absolute bottom-0 flex items-center justify-center w-16 h-16 border rounded-full shadow-md bg-gray-950 left-4 shadow-red-500/60 active:shadow-red-500 border-red-600/40 active:border-red-600/60'
       >
-        <img src={closeIcon} width='48' height='48' alt='Gettone Fato' />
+        <SvgClose width='48' height='48' className='fill-red-800' />
       </button>
     </>
   );
--- src/components/ToggleFateToken.tsx
+++ src/components/ToggleFateToken.tsx
@@ -1,17 +1,19 @@
 import { useState } from 'react';
-import fateOff from '../assets/flash_off.svg';
-import fateOn from '../assets/flash_on.svg';
-import fateIcon from '../assets/thunderstorm.svg';
+import { SvgFlashOff } from './SvgFlashOff';
+import { SvgFlashOn } from './SvgFlashOn';
+import { SvgThunderstorm } from './SvgThunderstorm';
 
 export const ToggleFateToken = () => {
   const [hasFateToken, toggleFateToken] = useState(false);
+
+  const FlashIcon = hasFateToken ? SvgFlashOff : SvgFlashOn;
 
   return (
     <>
       {hasFateToken && (
         <div className='absolute top-0 bottom-0 left-0 right-0 flex items-center justify-center'>
           <div className='flex items-center justify-center rounded-full shadow-lg w-36 h-36 bg-slate-50/90 shadow-yellow-500/30'>
-            <img src={fateIcon} width='88' height='88' alt='Gettone Fato' />
+            <SvgThunderstorm width='88' height='88' className='fill-amber-400' />
           </div>
         </div>
       )}
@@ -19,7 +21,7 @@ export const ToggleFateToken = () => {
         onClick={() => toggleFateToken(!hasFateToken)}
         className='absolute bottom-0 flex items-center justify-center w-16 h-16 rounded-full shadow-md bg-slate-50 active:shadow-yellow-500 right-4 shadow-yellow-500/60 active:bg-slate-100'
       >
-        <img src={hasFateToken ? fateOff : fateOn} width='48' height='48' alt='Gettone Fato' />
+        <FlashIcon width='48' height='48' className='fill-amber-400' />
       </button>
     </>
   );
