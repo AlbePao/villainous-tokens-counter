927dad0d09782b376b10859de1a32cb456c9dd80
Alberto Paolucci <a.paolucci@wematica.it>
2024-10-16 12:16:57 +0200

    move useWakeLock

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,21 @@
 import '@fontsource/pirata-one';
 import { useState } from 'react';
-import { useWakeLock } from 'react-screen-wake-lock';
 import { VillainCard } from './components/VillainCard';
 import { VillainDashboard } from './components/VillainDashboard';
 import { Villain } from './models/villain';
 import { villains } from './models/villains-list';
 
 function App() {
-  const { request, release } = useWakeLock();
-
   const [villain, setVillain] = useState<Villain | null>(null);
-
-  const aaaVillain = (villain: Villain | null) => {
-    if (villain) {
-      request();
-    } else {
-      release();
-    }
-
-    setVillain(villain);
-  };
 
   return (
     <>
       {villain ? (
-        <VillainDashboard {...villain} onCancel={() => aaaVillain(null)} />
+        <VillainDashboard {...villain} onCancel={() => setVillain(null)} />
       ) : (
         <div className='w-full mx-auto md:max-w-[67.5rem] grid grid-cols-2  md:grid-cols-3'>
           {villains.map((villain) => (
-            <VillainCard key={villain.name} {...villain} onClick={() => aaaVillain(villain)} />
+            <VillainCard key={villain.image} {...villain} onClick={() => setVillain(villain)} />
           ))}
         </div>
       )}
