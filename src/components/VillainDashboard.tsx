277b1c4470ea04c8ee850deab1ddda7b40bff3d1
Alberto Paolucci <a.paolucci@wematica.it>
2024-10-16 13:14:47 +0200

    flex

--- src/components/VillainDashboard.tsx
+++ src/components/VillainDashboard.tsx
@@ -26,7 +26,7 @@ export const VillainDashboard = (props: VillainProps) => {
       className='w-full mx-auto md:m-auto md:max-w-[22.5rem] flex flex-col items-start md:items-center justify-start md:justify-center bg-contain bg-no-repeat md:min-h-[512px]'
       style={{ backgroundImage: `url(${image})` }}
     >
-      <div className='flex flex-col justify-center flex-grow w-full gap-4 bg-gradient-to-t from-black from-50%'>
+      <div className='flex flex-col justify-center w-full gap-4 bg-gradient-to-t from-black from-50%'>
         <UpperButtons onCancel={onCancel} />
         <TokensCounter />
         {customToken && <TokensCounter customToken={customToken} />}
