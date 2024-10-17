3059ab0d4fdb1f74f7a878868d1b6024e9b19613
Alberto Paolucci <a.paolucci@wematica.it>
2024-10-17 12:05:23 +0200

    iosPWASplash

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,9 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
+import { iosPWASplash } from './ios-pwa-splash.ts';
+
+iosPWASplash('/android-chrome-512x512.png', '#0a0a0a');
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
